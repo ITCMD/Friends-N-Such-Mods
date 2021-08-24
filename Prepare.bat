b5278875fcec545162a8153dba4b2ba3ecbb95ae
Lucas Elliott <lucas_github@itcommand.net>
2021-08-24 12:51:56 -0400

    fixed crash

--- Prepare.bat
+++ Prepare.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Removing old mods from %~1\mods\
 if exist "%~1\mods\*.jar" (
-	echo Deleting old version of optifine . . .
+	echo Deleting old version of mods . . .
 	del /f /q "%~1\mods\*.jar"
 )
